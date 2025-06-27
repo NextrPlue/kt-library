903757701030c1f57f0b9d807cf1fe7d703c889e
NexTR_Plue <p.plue1881@gmail.com>
2025-06-27 20:55:49 +0900

    feat: 이메일로 작가 조회 메서드 추가

--- author/src/main/java/ktlibrary/domain/AuthorRepository.java
+++ author/src/main/java/ktlibrary/domain/AuthorRepository.java
@@ -4,7 +4,13 @@ import ktlibrary.domain.*;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
 //<<< PoEAA / Repository
 @RepositoryRestResource(collectionResourceRel = "authors", path = "authors")
 public interface AuthorRepository
-    extends PagingAndSortingRepository<Author, Long> {}
+    extends PagingAndSortingRepository<Author, Long> {
+
+    // 이메일로 작가 조회
+    Optional<Author> findByEmail(String email);
+}
