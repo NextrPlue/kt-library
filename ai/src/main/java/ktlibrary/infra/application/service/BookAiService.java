d660d8e67be614ffb2f4d6cdfb529d679c6a4d8f
Minsu Kim <minsue9608@naver.com>
2025-06-30 02:36:41 +0000

    feat: 카테고리 생성 메서드 추가

--- ai/src/main/java/ktlibrary/infra/application/service/BookAiService.java
+++ ai/src/main/java/ktlibrary/infra/application/service/BookAiService.java
@@ -10,6 +10,7 @@ public class BookAiService {
 
     private final SummaryService summaryService;
     private final CoverService coverService;
+    private final CategoryService categoryService;
 
     // 1. 원고 요약 생성
     public String generateSummary(GenerateSummaryCommand generateSummaryCommand) {
@@ -32,11 +33,13 @@ public class BookAiService {
 
         return coverService.generateCoverImage(title, summary);
     }
-//
-//    // 3. 카테고리 자동 분류
-//    public String classifyCategory(String summary) {
-//
-//    }
+
+    // 3. 카테고리 생성
+    public String generateCategory(GenerateSummaryCommand generateSummaryCommand) {
+        String summary = generateSummaryCommand.getSummary();
+
+        return categoryService.generateCategory(summary);
+    }
 //
 //    // 4. 최종 도서 등록
 //    public void registerBook(BookRegisterCommand bookRegisterCommand) {
