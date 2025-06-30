79f32c03d2de863ec9b858c27208b123edbdd9dd
Minsu Kim <minsue9608@naver.com>
2025-06-30 04:36:58 +0000

    fix: 커버 커맨드 수정

--- ai/src/main/java/ktlibrary/domain/Command/GenerateCoverCommand.java
+++ ai/src/main/java/ktlibrary/domain/Command/GenerateCoverCommand.java
@@ -5,12 +5,7 @@ import java.util.*;
 import lombok.Data;
 
 @Data
-public class GenerateCoverCommand {
-
-    private Long id;                    // 도서 ID
-    private String manuscriptTitle;     // 원고 제목
-    private String manuscriptContent;   // 원고 내용
-    private Long authorId;              // 작가 ID
-    private String authorName;          // 작가 이름
-    private String introduction;        // 작가 소개
+public class GenerateCoverCommand extends BaseManuscriptCommand {
+    private String summary;     // 요약 내용
+    private String coverUrl;    // 커버 이미지 url
 }
