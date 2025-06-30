153c96f02b345c8e6a1cfa2004be68057a6b5d48
angry nison <kalina01255@naver.com>
2025-06-30 06:34:10 +0000

    1. feat : 출간처리 시작 Policy 구현 PolicyHandler -> Service 실행으로 변경 StartPublishingService,StartPublishingCommand 파일 추가 Book aggregate 인스턴스 메서드 추가 BookNotFoundException 예외처리 class 추가
    2. refactor : manuscript 원고 조건 추가
    
    원고의 생성, 수정은 허가된 작가들만 할 수 있게 생성
    원고의 발행(publish)는 생성된 원고가 있어야지만 가능

--- ai/src/main/java/ktlibrary/infra/PolicyHandler.java
+++ ai/src/main/java/ktlibrary/infra/PolicyHandler.java
@@ -2,23 +2,22 @@ package ktlibrary.infra;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import javax.naming.NameParser;
-import javax.naming.NameParser;
 import javax.transaction.Transactional;
 import ktlibrary.config.kafka.KafkaProcessor;
-import ktlibrary.domain.*;
+import ktlibrary.domain.PublishingRequested;
+import ktlibrary.application.StartPublishingService;  
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
-//<<< Clean Arch / Inbound Adaptor
 @Service
 @Transactional
 public class PolicyHandler {
 
     @Autowired
-    BookRepository bookRepository;
+    private StartPublishingService startPublishingService; 
 
     @StreamListener(KafkaProcessor.INPUT)
     public void whatever(@Payload String eventString) {}
@@ -30,15 +29,12 @@ public class PolicyHandler {
     public void wheneverPublishingRequested_PublishingStarted(
         @Payload PublishingRequested publishingRequested
     ) {
-        PublishingRequested event = publishingRequested;
         System.out.println(
             "\n\n##### listener PublishingStarted : " +
-            publishingRequested +
+            publishingRequested + " 출간요청됨 수신 완료" +
             "\n\n"
         );
 
-        // Sample Logic //
-        Book.publishingStarted(event);
+        startPublishingService.handlePublishingRequest(publishingRequested);
     }
 }
-//>>> Clean Arch / Inbound Adaptor
