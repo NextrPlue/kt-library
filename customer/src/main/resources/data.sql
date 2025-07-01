eb7583046a7386ce9080126ac637d55c0be82696
BaeSJ1 1 <bsj9278@gmail.com>
2025-07-01 13:48:30 +0000

    fix: customer 테스트 데이터 문제 발생으로 주석 처리

--- customer/src/main/resources/data.sql
+++ customer/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 -- Customer 테스트 데이터
-INSERT INTO customer_table (id, name, email, password, is_kt_user, created_at, updated_at) 
-VALUES (1, '고객', 'customer@kt.com', 'password', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+-- INSERT INTO customer_table (id, name, email, password, is_kt_user, created_at, updated_at) 
+-- VALUES (1, '고객', 'customer@kt.com', 'password', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
---
+++ point/src/main/resources/data.sql
@@ -0,0 +1,19 @@
+-- 고객 ID 1번에 대해 포인트 데이터 삽입 (초기 6,000P)
+-- INSERT INTO point_table (
+--     id,              -- PK (Point Entity에서 @GeneratedValue가 아니라면 반드시 지정)
+--     customer_id,     -- 고객 ID (FK 아님, 일반 컬럼)
+--     point,           -- 보유 포인트
+--     book_id,         -- null 가능
+--     price,           -- null 가능
+--     create_at,
+--     update_at
+-- )
+-- VALUES (
+--     1,               -- ID (직접 지정)
+--     1,               -- customer_id (더미 고객)
+--     6000,            -- 초기 포인트
+--     NULL,
+--     NULL,
+--     CURRENT_TIMESTAMP,
+--     CURRENT_TIMESTAMP
+-- );
