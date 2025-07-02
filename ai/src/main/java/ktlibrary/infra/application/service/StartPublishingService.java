da4354817a93bc2821508813b2d996fceb31cea4
Minsu Kim <minsue9608@naver.com>
2025-07-02 05:59:42 +0000

    fix: import 오류 수정

--- ai/src/main/java/ktlibrary/infra/application/service/StartPublishingService.java
+++ ai/src/main/java/ktlibrary/infra/application/service/StartPublishingService.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-// import ktlibrary.domain.PublishingStarted;
+import ktlibrary.domain.PublishingStarted;
 
 @Service
 @Transactional
