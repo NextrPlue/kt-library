ef43c34c59e4593cd0e9466e0e12c3ee5807e4af
K-Minsu <79684170+K-Minsu@users.noreply.github.com>
2025-07-04 02:20:51 +0000

    fix: RegisterationRequested 속성 수정

--- platform/src/main/java/ktlibrary/domain/RegisterationRequested.java
+++ platform/src/main/java/ktlibrary/domain/RegisterationRequested.java
@@ -8,21 +8,20 @@ import lombok.*;
 @Data
 @ToString
 public class RegisterationRequested extends AbstractEvent {
+    private Long authorId;              // 작가 id
+    private String authorName;          // 작가 이름
+    private String introduction;        // 작가 소개
 
-    private Long id;
-    private Long bookId;
-    private String title;
-    private String fileUrl;
-    private String summary;
-    private String coverUrl;
-    private String bookUrl;
+    private Long id;                    // 도서 id
+    private String manuscriptTitle;     // 원고 제목
+    private String manuscriptContent;   // 원고 내용
+
+    private String summary;             // 도서 요약
+    private String coverUrl;            // 도서 커버 URL
+    private String bookUrl;             // 도서 파일 URL
+    private String category;            // 도서 카테고리
+    private Long price;                 // 도서 가격
+
     private Date createdAt;
     private Date updatedAt;
-    private String manuscriptTitle;
-    private String manuscriptContent;
-    private Long authorId;
-    private String authorName;
-    private String introduction;
-    private String category;
-    private Long price;
 }
